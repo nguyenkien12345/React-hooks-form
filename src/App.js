4770330a53e7d912130c51e49574ede451a6d4e3
nguyenkien <nguyenkien11202000@gmail.com>
2021-09-08 22:37:35 +0700

    Update Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
 function App() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const [userInfo, setUserInfo] = useState();
+
+  const onHandleSubmit = (data) => {
+    setUserInfo(data);
+  }
+
+  // Debug errors
+  // console.log(errors);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      {/* Debug here */}
+      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+
+      <form onSubmit={handleSubmit(onHandleSubmit)}>
+        <h1>Registration Form</h1>
+        <div className='ui divider'></div>
+        <div className='ui form'>
+          <div className='field'>
+            <label>Username</label>
+            <input type='text' name='username' placeholder='Name: ' {...register('username', { required: "Username is required" })}/>
+          </div>
+          <p>{errors.username?.message}</p>
+          <div className='field'>
+            <label>Email</label>
+            <input type='email' name='email' placeholder='Email: '  {...register('email', { required: "Email is required", pattern:{value: /^\S+@\S+$/i, message: "This is not a valid email"}})}/>
+          </div>
+          <p>{errors.email?.message}</p>
+          <div className='field'>
+            <label>Password</label>
+            <input type='password' name='password' placeholder='Password: ' {...register('password', { required: "Password is required", minLength:{value: 4, message:  "Password must be more than 4 characters" }, maxLength:{value: 64, message: "Passwords can not exceed than 64 characters"} })}/>
+          </div>
+          <p>{errors.password?.message}</p>
+          <button className='fluid ui button blue'>Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
